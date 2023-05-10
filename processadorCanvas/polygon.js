711cd59a185f19bd240c46ae8b1c0191c8991ec6
lbo21 <leonardobecker79@gmail.com>
2023-05-09 23:09:15 -0300

    movimentos

--- processadorCanvas/polygon.js
+++ processadorCanvas/polygon.js
@@ -1,19 +1,29 @@
 var canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
 
-point1 = {}
-point2 = {}
-point1.x = 20;
-point1.y = 20;
-point2.x = 550;
-point2.y = 500;
+reta = {}
+reta.x1 = 20;
+reta.y1 = 20;
+reta.x2 = 550;
+reta.y2 = 500;
+allRetas = []
+allRetas.push(reta);
 
-function getEquation(point1, point2) {
+for (i = 0; i < 10; i++) {
+    reta = {}
+    reta.x1 = Math.floor(Math.random() * 500)
+    reta.y1 = Math.floor(Math.random() * 500)
+    reta.x2 = Math.floor(Math.random() * 500)
+    reta.y2 = Math.floor(Math.random() * 500)
+    allRetas.push(reta)
+}
+
+function getEquation(reta) {
     // Calcula o coeficiente angular (slope) da reta
-    const slope = (point2.y - point1.y) / (point2.x - point1.x);
+    const slope = (reta.y2 - reta.y1) / (reta.x2 - reta.x1);
 
     // Calcula o coeficiente linear (y-intercept) da reta
-    const yIntercept = point1.y - slope * point1.x;
+    const yIntercept = reta.y1 - slope * reta.x1;
 
     return { slope, yIntercept };
 }
@@ -21,77 +31,109 @@ function getEquation(point1, point2) {
 isMouseDown = false;
 canto = 0;
 
-function drawLine(event) {
-    console.log("DOWN");
-    const x = event.clientX - canvas.offsetLeft;
-    const y = event.clientY - canvas.offsetTop;
+function drawLine(allRetas,indice) {
     ctx.beginPath();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (canto == 2) {
-        console.log("canto 2")
-        ctx.moveTo(point1.x, point1.y);
-        point2.x = x;
-        point2.y = y;
-        ctx.lineTo(point2.x, point2.y);
-    }
-    else if (canto == 1) {
-        console.log("canto 1")
-        ctx.moveTo(point2.x, point2.y);
-        point1.x = x;
-        point1.y = y;
-        ctx.lineTo(point1.x, point1.y);
-    }
+    ctx.moveTo(allRetas[indice].x1, allRetas[indice].y1);
+    ctx.lineTo(allRetas[indice].x2, allRetas[indice].y2);
     ctx.strokeStyle = "black";
     ctx.lineWidth = 4;
     ctx.stroke();
 }
 
+function drawCircle(allRetas,indice){
+    ctx.beginPath();
+    ctx.arc(allRetas[indice].x1, allRetas[indice].y1, 5, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(allRetas[indice].x2, allRetas[indice].y2, 5, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.stroke();
+}
+
+for (i in allRetas) {
+    drawLine(allRetas,i);
+    drawCircle(allRetas,i);
+}
+
+function moveLine(event, allRetas, indiceReta) {
+    const x = event.clientX - canvas.offsetLeft;
+    const y = event.clientY - canvas.offsetTop;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (i in allRetas) {
+        if (indiceReta.includes(i)) {
+            console.log("oiii")
+            ctx.beginPath();
+            if (canto == 2) {
+                ctx.moveTo(allRetas[i].x1, allRetas[i].y1);
+                allRetas[i].x2 = x;
+                allRetas[i].y2 = y;
+                ctx.lineTo(allRetas[i].x2, allRetas[i].y2);
+            }
+            else if (canto == 1) {
+                ctx.moveTo(allRetas[i].x2, allRetas[i].y2);
+                allRetas[i].x1 = x;
+                allRetas[i].y1 = y;
+                ctx.lineTo(allRetas[i].x1, allRetas[i].y1);
+            }
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 4;
+            ctx.stroke();
+            drawCircle(allRetas,i);
+        }
+        else 
+        {
+            drawLine(allRetas,i)
+            drawCircle(allRetas,i)
+        }
+    }
+}
+
+retaSelecionada=[]
+
 canvas.addEventListener("mousedown", function (event) {
     const x = event.clientX - canvas.offsetLeft;
     const y = event.clientY - canvas.offsetTop;
-    param = getEquation(point1, point2);
     const tolerancia = 8;
-    const distancia = Math.abs(x * param.slope + param.yIntercept - y);
 
-    const diffX1 = Math.abs(x - point1.x);
-    const diffY1 = Math.abs(y - point1.y);
-    console.log(diffX1, diffY1)
+    for (i in allRetas) {
+        param = getEquation(allRetas[i]);
+        const distancia = Math.abs(x * param.slope + param.yIntercept - y);
 
-    const diffX2 = Math.abs(x - point2.x);
-    const diffY2 = Math.abs(y - point2.y);
+        const diffX1 = Math.abs(x - allRetas[i].x1);
+        const diffY1 = Math.abs(y - allRetas[i].y1);
 
-    if (distancia <= tolerancia)
-        console.log("reta");
-    if (diffX2 <= tolerancia && diffY2 <= tolerancia) {
-        canto = 2;
-        isMouseDown = true;
-    }
-    else if (diffX1 <= tolerancia && diffY1 <= tolerancia) {
-        canto = 1;
-        isMouseDown = true;
-    }
-    if (isMouseDown) {
-        drawLine(event);
+        const diffX2 = Math.abs(x - allRetas[i].x2);
+        const diffY2 = Math.abs(y - allRetas[i].y2);
+
+        if (distancia <= tolerancia)
+            console.log("reta");
+        if (diffX2 <= tolerancia && diffY2 <= tolerancia) {
+            canto = 2;
+            isMouseDown = true;
+            retaSelecionada.push(i)
+        }
+        else if (diffX1 <= tolerancia && diffY1 <= tolerancia) {
+            canto = 1;
+            isMouseDown = true;
+            retaSelecionada.push(i)
+        }
+        if (isMouseDown) {
+            console.log(retaSelecionada)
+            moveLine(event, allRetas, retaSelecionada);
+        }
     }
 });
 
 canvas.addEventListener("mousemove", function (e) {
     if (isMouseDown)
-        drawLine(e);
+        moveLine(e, allRetas, retaSelecionada);
 });
 
 canvas.addEventListener("mouseup", function (e) {
     console.log("UP");
     isMouseDown = false;
     console.log(isMouseDown);
+    retaSelecionada=[];
 });
-
-
-ctx.beginPath();
-
-ctx.moveTo(point1.x, point1.y);
-ctx.lineTo(point2.x, point2.y);
-ctx.strokeStyle = "green";
-ctx.lineWidth = 4;
-
-ctx.stroke();
