431ca6050a6ac2d81da59dcf712b7fba228a23cb
lbo21 <leonardobecker79@gmail.com>
2023-05-17 23:54:40 -0300

    ok

---
+++ sitePessoal/javaScript/visualizerScript.js
@@ -0,0 +1,247 @@
+var url = "./database/alunos.xml";
+
+$(document).keydown(function (event) {
+    if (event.which === 13) {
+        aluno = ($("#grr").val());
+        loadDoc();
+    }
+});
+
+//Arruma o modal no tamanho da tela
+$(document).ready(function() {
+    var largura = $("nav").width();
+    var margemEsquerda = parseInt($("nav").css("padding-left"));
+    var margemDireita = parseInt($("nav").css("padding-right"));
+    var larguraTela = $(window).width();
+
+    var tamanhoNav=largura+margemDireita+margemEsquerda;
+    $(".modal").css({'left':tamanhoNav,'width':(larguraTela-tamanhoNav)})
+});
+
+
+let codigo;
+let vetorr = [];
+let allMatriculas = [];
+var aluno = "";
+var attMatricula = 0;
+
+$(document).ready(function () {
+    $(function () {
+        $(".autocomplete").autocomplete({
+            source: allMatriculas,
+            select:
+                function (event, ui) {
+                    aluno = ui.item.value;
+                    loadDoc();
+                }
+        });
+    });
+});
+
+$(document).ready(function () {
+    loadDoc();
+});
+
+
+function updateMatriculas() {
+    if (attMatricula == 0) {
+        for (i = 0; i < allMatriculas.length; i++) {
+            info = "<option>" + allMatriculas[i] + "</option>"
+            $("#stds").append(info);
+        }
+        attMatricula = 1
+    }
+}
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this);
+            updateMatriculas();
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+
+function limpaTabela() {
+    grade = $("td").removeClass();
+    grade = $("td").addClass("none");
+}
+
+function tabelaInvalida() {
+    grade = $("td").removeClass();
+}
+
+
+function myFunction(xml) {
+    var i;
+    var xmlDoc = xml.responseXML;
+
+    var x = xmlDoc.getElementsByTagName("ALUNO");
+
+    vetorGrade = []
+    grade = $("#grade td");
+
+
+    if (allMatriculas.includes(aluno)) {
+        $("#alunoAtual").remove();
+        $("#aluno").append("<h4 id='alunoAtual'>Aluno: " + aluno + "</h4>")
+        limpaTabela();
+    }
+    else {
+        $("#alunoAtual").remove();
+        tabelaInvalida();
+    }
+    for (i = 0; i < grade.length; i++) {
+        vetorGrade.push($(grade[i]).find("b").text());
+    }
+    vetorr = [];
+    for (i = 0; i < x.length; i++) {
+
+        matricula = x[i].getElementsByTagName("MATR_ALUNO")[0].childNodes[0].nodeValue;
+        if (!allMatriculas.includes(matricula))
+            allMatriculas.push(matricula);
+
+        if (matricula == aluno) {
+
+            codigo = x[i].getElementsByTagName("COD_ATIV_CURRIC")[0].childNodes[0].nodeValue;
+            nomeDiscplina = x[i].getElementsByTagName("NOME_ATIV_CURRIC")[0].childNodes[0].nodeValue;
+            ano = x[i].getElementsByTagName("ANO")[0].childNodes[0].nodeValue;
+            periodo = x[i].getElementsByTagName("PERIODO")[0].childNodes[0].nodeValue;
+            situacao = x[i].getElementsByTagName("SITUACAO")[0].childNodes[0].nodeValue;
+            nota = x[i].getElementsByTagName("MEDIA_FINAL")[0].childNodes[0].nodeValue;
+            frequencia = x[i].getElementsByTagName("FREQUENCIA")[0].childNodes[0].nodeValue;
+            frequencia = parseFloat(frequencia).toFixed(2);
+            atv = x[i].getElementsByTagName("DESCR_ESTRUTURA")[0].childNodes[0].nodeValue;
+
+            if (atv == "Trabalho de Graduação I") {
+                codigo = "TG I"
+            }
+            else if (atv == "Trabalho de Graduação II") {
+                codigo = "TG II"
+            }
+
+            for (j = 0; j < vetorGrade.length; j++) {
+
+                if ((codigo == vetorGrade[j])) {
+
+                    vetorr.push([codigo, nomeDiscplina, ano, periodo, situacao, nota, frequencia]);
+
+                    ft = $(grade[j]);
+                    if ((situacao == "Reprovado por nota") || (situacao == "Reprovado por Frequência")) {
+                        ft.removeClass();
+                        ft.addClass("reprovado");
+                    }
+                    else if (situacao == "Aprovado") {
+                        ft.removeClass();
+                        ft.addClass("aprovado");
+                    }
+                    else if (situacao == "Matrícula") {
+                        ft.removeClass();
+                        ft.addClass("matricula");
+                    }
+                    else if ((situacao == "Equivalência de Disciplina") || (situacao == "Dispensa de Disciplinas (com nota)")) {
+                        ft.removeClass();
+                        ft.addClass("equivalencia")
+                    }
+                }
+            }
+
+        }
+    }
+    vetorr.sort();
+}
+
+
+$(document).ready(function () {
+
+    $("td").click(function () {
+        codigo = ($(this).find("b").text());
+        vetEspecifico = [];
+        for (i = 0; i < vetorr.length; i++) {
+            if (vetorr[i][0] == codigo) {
+                vetEspecifico.push(vetorr[i]);
+            }
+        }
+        dado = vetEspecifico[(vetEspecifico.length - 1)];
+        if (dado == null) {
+            $("#clickHeader").hide();
+            $("#clickBody").hide();
+            $(".modal-content table").hide();
+            $(".modal-content p").remove();
+            $(".modal-content").append("<p>Histórico inexistente</p>")
+        }
+        else {
+            $("#clickBody").empty();
+            $(".modal-content p").remove();
+            $(".modal-content table").show();
+            info = "<tr>"
+            for (i = 0; i < dado.length; i++) {
+                info = info + "<td>" + dado[i] + "</td>"
+            }
+            info = info + "</tr>"
+            $("#clickBody").append(info);
+            $("#clickHeader").show();
+            $("#clickBody").show();
+        }
+        $("#modal").show();
+
+    });
+
+    $("td").on("contextmenu", function (event) {
+        event.preventDefault();
+        codigo = $(this).find("b").text();
+        vetEspecifico = [];
+        for (i = 0; i < vetorr.length; i++) {
+            if (vetorr[i][0] == codigo) {
+                vetEspecifico.push(vetorr[i]);
+            }
+        }
+        if ((vetEspecifico.length) == 0) {
+            $("#clickHeader").hide();
+            $("#clickBody").hide();
+            $(".modal-content table").hide();
+            $(".modal-content p").remove();
+            $(".modal-content").append("<p>Histórico inexistente</p>")
+        }
+        else {
+            $("#clickBody").empty();
+            $(".modal-content p").remove();
+            $(".modal-content table").show();
+            for (i = 0; i < vetEspecifico.length; i++) {
+                info = "<tr>"
+                dado = vetEspecifico[i];
+                for (j = 0; j < dado.length; j++) {
+                    info = info + "<td>" + dado[j] + "</td>"
+                }
+                info = info + "</tr>"
+                $("#clickBody").append(info);
+            }
+            $("#clickHeader").show();
+            $("#clickBody").show();
+        }
+        $("#modal").show();
+    });
+});
+
+$(document).ready(function () {
+
+    $("#closeModal").click(function () {
+        $("#modal").hide();
+    });
+
+    $(document).keyup(function (event) {
+        if (event.key === "Escape") {
+            $("#modal").hide();
+        }
+    });
+
+    $(window).click(function (event) {
+        if (event.target == $("#modal")[0]) {
+            $("#modal").hide();
+        }
+    });
+});
